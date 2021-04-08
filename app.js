bcf82f26dc646e5026ec509d6e8bf0363e509afa
colineckert <colineckert10@gmail.com>
2021-04-08 11:34:06 -0700

    connect to mongo

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send("This works!"));
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongo's a gogo"))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send("This works, huzzah!"));
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is serving on port ${port}`));
